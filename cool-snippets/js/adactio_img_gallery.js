51ce30ac48c5f87f140dbcb0c645abaa1f020ba0
Microspino <info@microspino.com>
2018-05-09 14:37:32 +0200

    add snippet for ultra simple and minimal gallery

---
+++ cool-snippets/js/adactio_img_gallery.js
@@ -0,0 +1,7 @@
+function showPic (pic) {
+  if !document.getElementById { return true; }
+  document.getElementById('placeholder').src = pic.href;
+  var desc = pic.title ? pic.title : pic.childNodes[0].nodeValue;
+  document.getElementById('desc').childNodes[0].nodeValue = desc;
+  return false;
+}
