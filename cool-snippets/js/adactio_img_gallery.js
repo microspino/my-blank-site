2aeec281730e1127b11be4e62bbf39a0d94ce1e8
Microspino <info@microspino.com>
2018-05-09 14:38:47 +0200

    cleanup

--- cool-snippets/js/adactio_img_gallery.js
+++ cool-snippets/js/adactio_img_gallery.js
@@ -1,5 +1,5 @@
 function showPic (pic) {
-  if !document.getElementById { return true; }
+  if (!document.getElementById) { return true; }
   document.getElementById('placeholder').src = pic.href;
   var desc = pic.title ? pic.title : pic.childNodes[0].nodeValue;
   document.getElementById('desc').childNodes[0].nodeValue = desc;
